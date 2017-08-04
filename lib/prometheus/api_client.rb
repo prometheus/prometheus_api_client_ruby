20b0ea0524965d0d60ed490be3ecdaea6a8c275c
yaacov <kobi.zamir@gmail.com>
2017-08-04 22:04:52 +0300

    add more options

--- lib/prometheus/api_client.rb
+++ lib/prometheus/api_client.rb
@@ -1,30 +1,63 @@
 # encoding: UTF-8
 
+require 'uri'
 require 'faraday'
 
 module Prometheus
   # Client is a ruby implementation for a Prometheus compatible api_client.
   module ApiClient
-    DEFAULT_URI = 'http://localhost:8080/'.freeze
+    DEFAULT_HOST = 'localhost'.freeze
+    DEFAULT_SCHEME = 'http'.freeze
+    DEFAULT_PORT = 9090
+    DEFAULT_PATH = '/api/v1/'.freeze
+    DEFAULT_CREDENTIALS = {}
+    DEFAULT_OPTIONS = {}
 
     # Returns a default client object
-    def self.client(uri = DEFAULT_URI, credentials = {}, options = {})
-      @args = { url: uri.to_s, request: { open_timeout: 2, timeout: 5 } }
-      @args.merge!(prometheus_args(credentials, options))
+    def self.client(
+      scheme = DEFAULT_SCHEME, host = DEFAULT_HOST, port = DEFAULT_PORT,
+      path = DEFAULT_PATH, credentials = DEFAULT_CREDENTIALS,
+      options = DEFAULT_OPTIONS)
 
-      Faraday.new(@args)
+      Faraday.new(
+        prometheus_args(scheme, host, port, path, credentials,options)
+      )
     end
 
-    def self.prometheus_args(credentials, options)
+    def self.prometheus_uri(scheme, host, port, path)
+      builder = {
+        http: URI::HTTP,
+        https: URI::HTTPS,
+      }[scheme.to_sym]
+
+      builder.build(
+        host: host,
+        port: port,
+        path: path,
+      )
+    end
+
+    def self.prometheus_args(scheme, host, port, path, credentials, options)
       {
-        proxy: options[:http_proxy_uri],
-        headers: { Authorization: 'Bearer ' + credentials[:token].to_s },
+        url: prometheus_uri(scheme, host, port, path).to_s,
+        proxy: if options[:http_proxy_uri]
+                 options[:http_proxy_uri]
+               end,
         ssl: if options[:verify_ssl]
                {
                  verify: options[:verify_ssl] != OpenSSL::SSL::VERIFY_NONE,
                  cert_store: options[:ssl_cert_store],
                }
              end,
+        headers: if credentials[:token]
+                   {
+                     Authorization: "Bearer " + credentials[:token],
+                   }
+                 end,
+        request: {
+          open_timeout: 2,
+          timeout: 5,
+        },
       }
     end
   end
