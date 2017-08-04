c3ef3b3ca9b5a292d45c4babb69dc88693126386
yaacov <kobi.zamir@gmail.com>
2017-08-04 22:22:53 +0300

    fix some code

--- lib/prometheus/api_client.rb
+++ lib/prometheus/api_client.rb
@@ -10,17 +10,18 @@ module Prometheus
     DEFAULT_SCHEME = 'http'.freeze
     DEFAULT_PORT = 9090
     DEFAULT_PATH = '/api/v1/'.freeze
-    DEFAULT_CREDENTIALS = {}
-    DEFAULT_OPTIONS = {}
+    DEFAULT_CREDENTIALS = {}.freeze
+    DEFAULT_OPTIONS = {}.freeze
 
     # Returns a default client object
     def self.client(
-      scheme = DEFAULT_SCHEME, host = DEFAULT_HOST, port = DEFAULT_PORT,
-      path = DEFAULT_PATH, credentials = DEFAULT_CREDENTIALS,
-      options = DEFAULT_OPTIONS)
+        scheme = DEFAULT_SCHEME, host = DEFAULT_HOST, port = DEFAULT_PORT,
+        path = DEFAULT_PATH, credentials = DEFAULT_CREDENTIALS,
+        options = DEFAULT_OPTIONS
+      )
 
       Faraday.new(
-        prometheus_args(scheme, host, port, path, credentials,options)
+        prometheus_args(scheme, host, port, path, credentials, options),
       )
     end
 
@@ -37,27 +38,34 @@ module Prometheus
       )
     end
 
+    def self.prometheus_proxy(options)
+      options[:http_proxy_uri] if options[:http_proxy_uri]
+    end
+
+    def self.prometheus_verify_ssl(options)
+      return unless options[:verify_ssl]
+
+      {
+        verify: options[:verify_ssl] != OpenSSL::SSL::VERIFY_NONE,
+        cert_store: options[:ssl_cert_store],
+      }
+    end
+
+    def self.prometheus_headers(credentials)
+      return unless credentials[:token]
+
+      {
+        Authorization: 'Bearer ' + credentials[:token].to_s
+      }
+    end
+
     def self.prometheus_args(scheme, host, port, path, credentials, options)
       {
         url: prometheus_uri(scheme, host, port, path).to_s,
-        proxy: if options[:http_proxy_uri]
-                 options[:http_proxy_uri]
-               end,
-        ssl: if options[:verify_ssl]
-               {
-                 verify: options[:verify_ssl] != OpenSSL::SSL::VERIFY_NONE,
-                 cert_store: options[:ssl_cert_store],
-               }
-             end,
-        headers: if credentials[:token]
-                   {
-                     Authorization: "Bearer " + credentials[:token],
-                   }
-                 end,
-        request: {
-          open_timeout: 2,
-          timeout: 5,
-        },
+        proxy: prometheus_proxy(options),
+        ssl: prometheus_verify_ssl(options),
+        headers: prometheus_headers(credentials),
+        request: {open_timeout: 2, timeout: 5},
       }
     end
   end
