0dafc9a81ea5d8876c6258d96eb452887a97ac56
Adam Renberg Tamm <adam.renbergtamm@shopify.com>
2024-06-14 16:15:26 +0200

    Handle HTTP API error responses
    Signed-off-by: Adam Renberg Tamm <adam.renbergtamm@shopify.com>

--- spec/prometheus/api_client/client_spec.rb
+++ spec/prometheus/api_client/client_spec.rb
@@ -44,4 +44,35 @@ describe Prometheus::ApiClient::Client do
       end
     end
   end
+
+  describe '.run_command' do
+    it 'returns a hash' do
+      VCR.use_cassette('prometheus/api_client/client') do # , record: :new_episodes) do
+        prometheus = Prometheus::ApiClient::Client.new(
+          url:         url,
+          credentials: { token: token },
+          options:     { verify_ssl: OpenSSL::SSL::VERIFY_NONE },
+        )
+
+        response = prometheus.run_command(
+          "query_range",
+          { query: "up", start: "2015-07-01T20:10:30.781Z", end: "2015-07-01T20:11:00.781Z", step: "15s"}
+        )
+
+        expect(response).to eq({ "resultType"=>"matrix", "result"=>[], "explanation" => nil })
+      end
+    end
+
+    it 'raises on error' do
+      VCR.use_cassette('prometheus/api_client/client') do # , record: :new_episodes) do
+        prometheus = Prometheus::ApiClient::Client.new(
+          url:         url,
+          credentials: { token: token },
+          options:     { verify_ssl: OpenSSL::SSL::VERIFY_NONE },
+        )
+
+        expect { prometheus.run_command("query_ranage", { query: "(not a valid command"}) } .to raise_error(Prometheus::ApiClient::Client::RequestError)
+      end
+    end
+  end
 end
